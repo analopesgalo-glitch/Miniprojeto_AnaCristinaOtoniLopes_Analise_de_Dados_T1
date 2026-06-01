8f4081da304176b9bef89be809f4f8590ebdaf14
analopesgalo-glitch <analopesgalo@gmail.com>
2026-06-01 15:02:02 -0300

    Sprint 2: conversão de tipos - DATA para datetime e remoção de colunas vazias

--- Miniprojeto_AnaCristina_Dados_T1.py
+++ Miniprojeto_AnaCristina_Dados_T1.py
@@ -6,3 +6,8 @@ df = pd.read_csv('Base Varejo.csv', sep=';')
 print (df.shape)
 print(df.dtypes)
 print(df.head())
+
+#sprint 2
+df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y')
+df = df.drop(columns=['Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13'])
+print (df.dtypes)
