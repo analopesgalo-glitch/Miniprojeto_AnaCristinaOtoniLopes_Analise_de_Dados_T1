cc62743a5308ce09ee795d221fe6c4b622bb4175
analopesgalo-glitch <analopesgalo@gmail.com>
2026-06-01 15:15:28 -0300

    Sprint 4: estatísticas descritivas da coluna número de filhos

--- Miniprojeto_AnaCristina_Dados_T1.py
+++ Miniprojeto_AnaCristina_Dados_T1.py
@@ -20,3 +20,15 @@ df = df.drop_duplicates()
 print(df.shape)
 df.to_csv('df_limpo.csv', index=False)
 print("\nArquivo df_limpo.csv salvo com sucesso!")
+
+#sprint 4
+print("Média:          ", df['CL_FHL'].mean())
+print("Mediana:        ", df['CL_FHL'].median())
+print("Desvio Padrão:  ", df['CL_FHL'].std())
+print("Moda:           ", df['CL_FHL'].mode()[0])
+print("Máximo:         ", df['CL_FHL'].max())
+print("Mínimo:         ", df['CL_FHL'].min())
+print("Contagem:       ", df['CL_FHL'].count())
+
+print("\n--- Quartis ---")
+print(df['CL_FHL'].quantile([0.25, 0.50, 0.75]))
