23a98bde79d5656d8cb374175afffa7bd865f552
analopesgalo-glitch <analopesgalo@gmail.com>
2026-06-01 15:33:07 -0300

    Sprint 5: agrupamentos por gênero, categoria e categoria x gênero

--- Miniprojeto_AnaCristina_Dados_T1.py
+++ Miniprojeto_AnaCristina_Dados_T1.py
@@ -17,6 +17,10 @@ print(df.isnull().sum())
 print(df.duplicated().sum())
 
 df = df.drop_duplicates()
+# Verifica valores únicos em CL_GENERO para identificar possíveis inconsistências
+print(df['CL_GENERO'].unique())
+print(df['CL_GENERO'].value_counts())
+df['PR_CAT'] = df['PR_CAT'].replace('#N/D', 'Sem Categoria')
 print(df.shape)
 df.to_csv('df_limpo.csv', index=False)
 print("\nArquivo df_limpo.csv salvo com sucesso!")
@@ -32,3 +36,8 @@ print("Contagem:       ", df['CL_FHL'].count())
 
 print("\n--- Quartis ---")
 print(df['CL_FHL'].quantile([0.25, 0.50, 0.75]))
+
+#sprint 5
+print(df.groupby('CL_GENERO')['CO_ID'].count().sort_values(ascending=False))
+print(df.groupby('PR_CAT')['CO_ID'].count().sort_values(ascending=False))
+print(df.groupby(['CL_GENERO', 'PR_CAT'])['CO_ID'].count().sort_values(ascending=False))
